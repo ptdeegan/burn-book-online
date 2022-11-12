2079fe6dc92727098f07310013ac63fc0a849ba2
BrennanMcl <bdmclaughlin02@gmail.com>
2022-11-12 11:35:47 -0500

    added viewpost to navbar as feed

--- app.py
+++ app.py
@@ -10,4 +10,10 @@ def home():
 
 @app.get('/profile')
 def profile():
+    # will need to fetch all post with users name while they are logged in
+    # Could fetch based on a User ID tied to their profile
     return render_template('profile.html')
+
+@app.get('/feed')
+def viewpost():
+    return render_template('viewpost.html')

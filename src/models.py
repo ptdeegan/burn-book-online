4efefa3dec4f05e3209c01be5a0d32b45868f525
BrennanMcl <bdmclaughlin02@gmail.com>
2022-12-06 18:21:06 -0500

    Re: Last commit, forgot to stage change

--- src/models.py
+++ src/models.py
@@ -12,14 +12,14 @@ class Users(db.Model):
     last_name = db.Column(db.String, nullable = False)
     email = db.Column(db.String, nullable = False)
 
-def __repr__(self) -> str:
-    return f'Users(user_id={self.user_id}, first_name={self.first_name}, last_name={self.last_name}, email={self.email}'
+    def __repr__(self) -> str:
+        return f'Users(user_id={self.user_id}, first_name={self.first_name}, last_name={self.last_name}, email={self.email}'
 
-def __init__(self, user_id:int, first_name:str, last_name:str, email:str ):
-    self.user_id = user_id
-    self.first_name = first_name
-    self.last_name = last_name
-    self.email = email
+    def __init__(self, user_id:int, first_name:str, last_name:str, email:str ):
+        self.user_id = user_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
 
 
 

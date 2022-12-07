04a3fbc64b41dae1cb9d9155742a51c10f91aa91
BrennanMcl <bdmclaughlin02@gmail.com>
2022-12-06 19:06:47 -0500

    Made Post Repository, will expand in future

--- src/repositories/post_repository.py
+++ src/repositories/post_repository.py
@@ -1,1 +1,15 @@
 from src.models import Posts, db
+
+class Post_Repository:
+    #gets all posts
+    def get_all_posts(self) -> list[Posts]:
+        all_posts = list[Posts] = Posts.query.all()
+        return all_posts
+
+    #gets single post based on ID
+    def get_post_by_id(self, post_id: int) -> Posts:
+        found_post : Posts = Posts.query.get_or_404(post_id)
+        return found_post
+
+#Singlton for other module use
+posts_repository_singlton = Post_Repository()

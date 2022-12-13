1b124a5d742a7e6a78619b29bd16d9be1303a3f2
Boone <aboone27@uncc.edu>
2022-12-12 20:44:30 -0500

    comment functionality hopeful

--- src/repositories/comment_repository.py
+++ src/repositories/comment_repository.py
@@ -2,8 +2,8 @@ from src.models import Comments, Posts, User_likes, db
 
 class Comment_Repository:
 
-    def get_comments(self) -> list[Comments]:
-        all_comments: list[Comments] = Comments.query.all()
+    def get_comments(self, post_id) -> list[Comments]:
+        all_comments: list[Comments] = Comments.query.filter_by(post_id=post_id).all()
         return all_comments
     
     def create_comment(self, user_id: int, post_id: int, body: str) -> Comments:
